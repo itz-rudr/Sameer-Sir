c9e5b0dd2a484cf83fb0fafac68e0a65e07eaae2
itz-rudr <rudrag26.2@gmail.com>
2025-11-04 21:04:26 +0530

    string explanation

---
+++ string_explain.cpp
@@ -0,0 +1,8 @@
+//Program to explain string
+#include<stdio.h>
+int main(){
+	char a[10];
+	printf("Enter name:\n");
+	scanf("%s",a);// %s is used to print or input a string
+	printf("Name: %s",a);
+}
