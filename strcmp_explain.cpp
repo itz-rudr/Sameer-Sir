44f1e49ab5f694205e4236b77f1a7d5de947904c
itz-rudr <rudrag26.2@gmail.com>
2025-11-05 00:28:56 +0530

    string compare explanation

---
+++ strcmp_explain.cpp
@@ -0,0 +1,16 @@
+//To explain strcmp function
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char a[20],b[20];
+	printf("Enter two strings:\n");
+	scanf("%s%s",a,b);
+	//pre-defined function to compare two strings
+	if(strcmp(a,b)==0){
+		printf("Both strings are similar.\n");
+	}else if(strcmp(a,b)>0){
+		printf("String 1 is greater than String 2\n");
+	}else{
+		printf("String 2 is greater than String 1\n");
+	}
+}
