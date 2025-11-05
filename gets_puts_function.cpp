5f17d3f8cae052b25df79eca00704d62986d147c
itz-rudr <rudrag26.2@gmail.com>
2025-11-06 01:12:46 +0530

    Gets and puts string function explanation

---
+++ gets_puts_function.cpp
@@ -0,0 +1,10 @@
+//To explain gets and puts function
+#include<stdio.h>
+int main(){
+	char a[10];
+	printf("Enter string:\n");
+	//pre-defined function to enter data in string
+	gets(a);
+	//pre-defined function to print data of string
+	puts(a);
+}
