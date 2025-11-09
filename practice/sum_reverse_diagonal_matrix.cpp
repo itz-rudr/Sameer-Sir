1e3ecd6e981a20e828842fda63c24ce1ec084633
itz-rudr <rudrag26.2@gmail.com>
2025-11-10 00:28:25 +0530

    to print reverse diagonal of matrix

---
+++ practice/sum_reverse_diagonal_matrix.cpp
@@ -0,0 +1,36 @@
+//To print sum of reverse diagonal of matrix
+#include<stdio.h>
+int main(){
+	int i,j,m,n,sum=0;
+	printf("Enter rows of matrix:\n");
+	scanf("%d",&m);
+	printf("Enter columns of matrix:\n");
+	scanf("%d",&n);
+	printf("Enter the elements of matrix:\n");
+	int a[m][n];
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Entered matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			printf("%4d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Reverse diagonal of matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			if(i+j==n-1){
+				printf("%4d",a[i][j]);
+				sum=sum+a[i][j];
+			}else{
+				printf("    ");
+			}
+		}
+		printf("\n");
+	}
+	printf("Sum of diagonal elements of matrix is: %d",sum);
+}
