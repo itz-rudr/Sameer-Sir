ef84b753e61f0e6f15f66fbbc0277e4527a1e115
itz-rudr <rudrag26.2@gmail.com>
2025-11-11 12:51:30 +0530

    To print elements of lower traingular matrix

---
+++ practice/print_lower_triangle_matrix.cpp
@@ -0,0 +1,32 @@
+//To print lower triangular matrix
+#include<stdio.h>
+int main(){
+	int i,j,m,n;
+	printf("Enter rows of matrix:\n");
+	scanf("%d",&m);
+	printf("Enter columns of matrix:\n");
+	scanf("%d",&n);
+	printf("Enter the elements of matrix:\n");
+	int a[m][n];
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Entered matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			printf("%4d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Lower triangular matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			if(i>=j){
+				printf("%4d",a[i][j]);
+			}
+		}
+		printf("\n");
+	}
+}
