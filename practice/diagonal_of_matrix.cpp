425af7f8063e5e607757a1aa22c7a02efbfec40a
itz-rudr <rudrag26.2@gmail.com>
2025-11-12 10:18:35 +0530

    To print diagonal elements of matrix

---
+++ practice/diagonal_of_matrix.cpp
@@ -0,0 +1,34 @@
+//To print diagonal of matrix
+#include<stdio.h>
+int main(){
+	int i,j,m,n;
+	printf("Enter rows of matrix:\n");
+	scanf("%d",&m);
+	printf("Enter columns of matrix:\n");
+	scanf("%d",&n);
+	printf("Enter the elements of matrix:\n");
+	int a[m][n];
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Entered matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			printf("%4d",a[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Diagonal of matrix:\n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			if(i==j){
+				printf("%4d",a[i][j]);
+			}else{
+				printf("    ");
+			}
+		}
+		printf("\n");
+	}
+}
