cc702541e2bace0e473ea93b3bacac5cfdf1dbdb
itz-rudr <rudrag26.2@gmail.com>
2025-11-20 22:00:42 +0530

    To explain how to input and print data in array using pointer

---
+++ array_input.cpp
@@ -0,0 +1,16 @@
+//To input and print n no.s using array pointer
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter size of array:\n");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter elements of array:\n");
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("Given array:\n");
+	for(int i=0;i<n;i++){
+		printf("%4d",a[i]);
+	}
+}
