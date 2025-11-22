b69e473a505a645b67c9d73411b559bfc3b26afe
itz-rudr <rudrag26.2@gmail.com>
2025-11-23 00:31:46 +0530

    header file containing all basic mathematical operations

---
+++ Headers/basic.h
@@ -0,0 +1,17 @@
+//To explain header file in c
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+int sum(int a,int b){
+	return a+b;
+}
+int square(int a){
+	return a*a;
+}
+float circle_area(float r){
+	return (3.14*r*r);
+}
+float triangle_area(float b,float h){
+	return (0.5*b*h);
+}
