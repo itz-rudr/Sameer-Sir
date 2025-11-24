b95fcc8fc937f491bb3affc68bb96f2b714483f2
itz-rudr <rudrag26.2@gmail.com>
2025-11-25 00:00:49 +0530

    to explain how to make a loop using pointer

---
+++ Loop_pointer_explain.cpp
@@ -0,0 +1,16 @@
+//To explain how to create a loop using a pointer
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter a size of array:\n");
+	scanf("%d",&n);
+	int a[n],*p;
+	printf("Enter elements of array:\n");
+	for(p=&a[0];p<&a[n];p++){
+		scanf("%d",p);
+	}
+	printf("Given array:\n");
+	for(p=&a[0];p<&a[n];p++){
+		printf("%4d",*p);
+	}
+}
