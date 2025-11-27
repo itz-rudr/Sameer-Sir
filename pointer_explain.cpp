271a98cf9482411b7e636ed8ecc701c46673c4e1
itz-rudr <rudrag26.2@gmail.com>
2025-11-27 19:07:58 +0530

    To explain pointer variable in c

---
+++ pointer_explain.cpp
@@ -0,0 +1,9 @@
+//To explain pointer variable in c
+#include<stdio.h>
+int main(){
+	int a;
+	a=5;
+	printf("Value of a is %d\n",a);
+	printf("Address of a is %u\n",&a);//Shows value in unsigned integer
+	printf("Address of a is %x\n",&a);//Shows value in hexadecimal
+}
